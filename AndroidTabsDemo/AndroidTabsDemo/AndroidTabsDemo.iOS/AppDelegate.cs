7f87ff856c716ba3b92d5f42f560c8505f92b3b1
jorgemhtdev <jorge@jorgemht.dev>
2018-07-08 11:06:45 +0200

    Initial Commit. Basic Test passed

---
+++ AndroidTabsDemo/AndroidTabsDemo/AndroidTabsDemo.iOS/AppDelegate.cs
@@ -0,0 +1,18 @@
+﻿namespace AndroidTabsDemo.iOS
+{
+    using Xamarin.Forms;
+    using Foundation;
+    using UIKit;
+
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
